4dfb19dc3cacfeaeafa5f21e67a3db42e2b7b5ec
cmaste148 <cmaste148@gmail.com>
2025-10-13 14:13:07 -0700

    Init Commit

---
+++ Deque.cpp
@@ -0,0 +1,5 @@
+//
+// Created by L00711689 on 10/13/2025.
+//
+
+#include "Deque.hpp"
---
+++ Deque.hpp
@@ -0,0 +1,8 @@
+//
+// Created by L00711689 on 10/13/2025.
+//
+
+#ifndef DEQUE_HPP
+#define DEQUE_HPP
+
+#endif //DEQUE_HPP
