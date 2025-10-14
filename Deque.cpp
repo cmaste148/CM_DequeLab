58389cb6291765c785f23118f02a3159692bc42e
cmaste <cmaste148@gmail.com>
2025-10-13 20:38:45 -0700

    Need to fix resize and implement fix for list

--- Deque.cpp
+++ Deque.cpp
@@ -3,3 +3,123 @@
 //
 
 #include "Deque.hpp"
+#include <iostream>
+#include <deque>
+
+Deque::Deque(int size) : size(size), count(0), head(-1), tail(-1)
+{
+	theDeque = new int [size];
+}
+
+Deque::~Deque()
+{
+	delete[] theDeque;
+}
+
+void Deque::addTail(int value)
+{
+	if (count >= size)
+	{
+		resize();
+	}
+	tail++;
+	tail %= size;
+	count++;
+	theDeque[tail] = value;
+
+}
+
+int Deque::removeHead()
+{
+	if (count == 0)
+	{
+		throw std::out_of_range("Array is empty in removeHead");
+	}
+	head++;
+	count--;
+	head %= size;
+	return theDeque[head];
+
+
+}
+
+std::string Deque::dumpArray()
+{
+	for (int i = 0; i < count; i++)
+	{
+		std::cout << theDeque[i] << " ";
+	}
+	return "";
+}
+
+void Deque::resize()
+{
+	int* biggerDeque = new int[size*2];
+	int afterTail = count - tail;
+	for (int i = 0; i < count; i++)
+	{
+
+		/*for (int j = 0; j < size; j++)
+		{
+			biggerDeque[j] = theDeque[(tail-j) % size];
+		}*/
+		if (i <= afterTail)
+		{
+			biggerDeque[i] = theDeque[(head+i) % size];
+		}
+		else
+		{
+			biggerDeque[i] = theDeque[i-afterTail+1];
+		}
+	}
+	delete[] theDeque;
+	theDeque = biggerDeque;
+	head = -1, tail = count;
+	size *= 2;
+}
+
+std::string Deque::listQueue()
+{
+	int afterTail = count - tail;
+	for (int i = 0; i < count; i++)
+	{
+
+		/*for (int j = 0; j < size; j++)
+		{
+			biggerDeque[j] = theDeque[(tail-j) % size];
+		}*/
+		if (i <= afterTail)
+		{
+			std::cout << theDeque[(head+i) % size];
+		}
+		else
+		{
+			std::cout << theDeque[i-afterTail+1];
+		}
+	}
+}
+
+bool Deque::isEmpty()
+{
+	if (tail == head && count == 0)
+	{
+		head, tail = 0;
+		return true;
+	}
+	return false;
+}
+
+void Deque::addHead(int value)
+{
+
+}
+
+int Deque::removeTail()
+{
+	return 7;
+}
+
+void Deque::solveThink(int *values, int numValues)
+{
+
+}
--- Deque.hpp
+++ Deque.hpp
@@ -5,4 +5,32 @@
 #ifndef DEQUE_HPP
 #define DEQUE_HPP
 
+#include <string>
+
+class Deque
+{
+	const static int SIZE = 20;
+	int size;
+	int count;
+	int head;
+	int tail;
+	int * theDeque;
+public:
+	Deque(int size = SIZE);
+	~Deque();
+	void addTail(int value);
+	int removeHead();
+	std::string dumpArray();
+	void resize();
+	std::string listQueue();
+	bool isEmpty();
+
+	// Advanced Methods
+	void addHead(int value);
+	int removeTail();
+
+	// Thinking Problem
+	void solveThink(int* values, int numValues);
+};
+
 #endif //DEQUE_HPP
